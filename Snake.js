844eb88a1a2cdaa21a15da4a83a3f137ac91a54c
Samuel Bejan <110773742+SamiBejan@users.noreply.github.com>
2023-04-19 14:15:31 +0300

    Moving the entire snake

--- Snake.js
+++ Snake.js
@@ -8,6 +8,7 @@ function createGrid() {
         grid[i] = new Array(21);
     }
 
+    //we create each row and cell
     for (let i = 1; i < grid.length; ++i) {
         const row = document.createElement("tr");
         for(let j = 1; j < grid.length; ++j) {
@@ -23,21 +24,56 @@ function createGrid() {
     }
 }
 
-let i = 9, j = 9;
-grid[i][j].classList.add("snake");
-
-function move(e) {
-    grid[i][j].classList.remove("snake");
-    if (e.key === "ArrowUp") {
-        --i;
-    } else if (e.key === "ArrowDown") {
-        ++i;
-    } else if (e.key === "ArrowLeft") {
-        --j;
-    } else if (e.key === "" || e.key === "ArrowRight") {
-        ++j;
-    }
-    grid[i][j].classList.add("snake");
+//we create the snake object
+let snake = new Array(7);
+for (let k = 0, j = 9; k < snake.length; ++k, --j) {
+    snake[k] =  {"dir": "ArrowRight", "line": 9, "col": j};
+    grid[snake[k].line][snake[k].col].classList.add("snake");
 }
 
-window.addEventListener("keydown", move);
+let turnPoints = new Array(0);
+
+//we create the new turnpoints and set the direction for the head snake
+function setDirection(e) {
+    if (e.key != snake[0].dir && (e.key === "ArrowUp" || e.key === "ArrowDown" || e.key === "ArrowLeft" || e.key === "ArrowRight") && 
+        !((e.key === "ArrowUp" || e.key === "ArrowDown") && (snake[0].dir === "ArrowUp" || snake[0].dir === "ArrowDown") ||
+        (e.key === "ArrowLeft" || e.key === "ArrowRight") && (snake[0].dir === "ArrowLeft" || snake[0].dir === "ArrowRight"))) {
+            turnPoints.unshift({"dir": e.key, "line": snake[0].line, "col": snake[0].col});
+            snake[0].dir = turnPoints[0].dir;           
+    }
+    move();
+    
+}
+
+function move() {
+    //We change the content of the cells through which the snake is moving
+    for (let k = 0; k < snake.length; ++k) {
+        grid[snake[k].line][snake[k].col].classList.remove("snake");
+        if (snake[k].dir === "ArrowUp") {
+            --snake[k].line;
+        } else if (snake[k].dir === "ArrowDown") {
+            ++snake[k].line;
+        } else if (snake[k].dir === "ArrowLeft") {
+            --snake[k].col;
+        } else if (snake[k].dir === "ArrowRight") {
+            ++snake[k].col;
+        }
+        grid[snake[k].line][snake[k].col].classList.add("snake");
+    }
+    //We give the new directions to the snake cells if they passed a turnpoint
+    for (let k = 1; k < snake.length; ++k) {
+        for (let l = 0, end = 0; l < turnPoints.length && !end; ++l) {
+            if (snake[k].line === turnPoints[l].line && snake[k].col === turnPoints[l].col) {
+                snake[k].dir = turnPoints[l].dir;
+                end = 1;
+            }          
+        }
+    }
+    //We delete the last turnpoint if the last snake cell passed it
+    if (turnPoints.length >= 1 && snake[snake.length - 1].line === turnPoints[turnPoints.length - 1].line && 
+        snake[snake.length - 1].col === turnPoints[turnPoints.length - 1].col) {
+            turnPoints.pop();
+    }
+}
+
+window.addEventListener("keydown", setDirection);
