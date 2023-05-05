6fa2d04b23b4b112420d4ff45a88cc92fd658519
Samuel Bejan <110773742+SamiBejan@users.noreply.github.com>
2023-05-06 00:00:38 +0300

    Update Snake.js
    Implemented an easier method that makes the snake to move; also added the start and end functionality.

--- Snake.js
+++ Snake.js
@@ -1,5 +1,8 @@
 const area = document.querySelector(".area");
+const startButton = document.querySelector(".start");
 let grid = new Array(21);
+let snake = new Array(7);
+let animate;
 
 createGrid();
 
@@ -24,64 +27,67 @@ function createGrid() {
     }
 }
 
-//we create the snake object
-let snake = new Array(7);
-for (let k = 0, j = 9; k < snake.length; ++k, --j) {
-    snake[k] =  {"dir": "ArrowRight", "line": 9, "col": j};
-    grid[snake[k].line][snake[k].col].classList.add("snake");
-}
+createSnake();
 
-let turnPoints = new Array(0);
-
-//we create the new turnpoints and set the direction for the head snake
-function setDirection(e) {
-    if (e.key != snake[0].dir && (e.key === "ArrowUp" || e.key === "ArrowDown" || e.key === "ArrowLeft" || e.key === "ArrowRight") && 
-        !((e.key === "ArrowUp" || e.key === "ArrowDown") && (snake[0].dir === "ArrowUp" || snake[0].dir === "ArrowDown") ||
-        (e.key === "ArrowLeft" || e.key === "ArrowRight") && (snake[0].dir === "ArrowLeft" || snake[0].dir === "ArrowRight"))) {
-            turnPoints.unshift({"dir": e.key, "line": snake[0].line, "col": snake[0].col});
-            snake[0].dir = turnPoints[0].dir;           
-    }   
-}
-
-var animate = setInterval(move, 100);
-
-function move() {
-    //We check to see if the next head cell in a specific direction will be outside the grid or in the snake body
-    if ((snake[0].dir === "ArrowUp" && (snake[0].line - 1 === 0 || grid[snake[0].line - 1][snake[0].col].classList.contains("snake"))) ||
-    (snake[0].dir === "ArrowDown" && (snake[0].line + 1 === 21 || grid[snake[0].line + 1][snake[0].col].classList.contains("snake"))) ||
-    (snake[0].dir === "ArrowLeft" && (snake[0].col - 1 === 0 || grid[snake[0].line][snake[0].col - 1].classList.contains("snake"))) ||
-    (snake[0].dir === "ArrowRight" && (snake[0].col + 1 === 21 || grid[snake[0].line][snake[0].col + 1].classList.contains("snake")))) {
-        clearInterval(animate);
-    } else {
-        //We change the content of the cells through which the snake is moving
-        for (let k = 0; k < snake.length; ++k) {
-            grid[snake[k].line][snake[k].col].classList.remove("snake");
-            if (snake[k].dir === "ArrowUp") {
-                    --snake[k].line;
-            } else if (snake[k].dir === "ArrowDown") {
-                    ++snake[k].line;
-            } else if (snake[k].dir === "ArrowLeft") {
-                    --snake[k].col;             
-            } else if (snake[k].dir === "ArrowRight") {
-                    ++snake[k].col;
-            }
-            grid[snake[k].line][snake[k].col].classList.add("snake");  
-        }
-        //We give the new directions to the snake cells if they passed a turnpoint
-        for (let k = 1; k < snake.length; ++k) {
-            for (let l = 0, end = 0; l < turnPoints.length && !end; ++l) {
-                if (snake[k].line === turnPoints[l].line && snake[k].col === turnPoints[l].col) {
-                    snake[k].dir = turnPoints[l].dir;
-                    end = 1;
-                }          
-            }
-        }
-        //We delete the last turnpoint if the last snake cell passed it
-        if (turnPoints.length >= 1 && snake[snake.length - 1].line === turnPoints[turnPoints.length - 1].line && 
-            snake[snake.length - 1].col === turnPoints[turnPoints.length - 1].col) {
-                turnPoints.pop();
-        }
+//We create the snake
+function createSnake() {
+    //We remove the previous snake if any
+    const previousSnake = document.getElementsByClassName("snake");
+    while (previousSnake.length) {
+        previousSnake[0].classList.remove("snake");
+    }
+    //We build the new snake
+    for (let k = 0, j = 9; k < snake.length; ++k, --j) {
+        snake[k] =  {"line": 9, "col": j};
+        grid[snake[k].line][snake[k].col].classList.add("snake");
     }
 }
 
-window.addEventListener("keydown", setDirection);
+function startGame() {
+    createSnake();
+    clearInterval(animate);
+    animate = setInterval(move, 200);
+    let dir = ["ArrowRight"];
+    function setDirection(e) {
+        if (e && e.key != dir[dir.length - 1] && (e.key === "ArrowUp" || e.key === "ArrowDown" || e.key === "ArrowLeft" || e.key === "ArrowRight") &&
+        !((e.key === "ArrowUp" || e.key === "ArrowDown") && (dir[dir.length - 1] === "ArrowUp" || dir[dir.length - 1] === "ArrowDown") ||
+        (e.key === "ArrowLeft" || e.key === "ArrowRight") && (dir[dir.length - 1] === "ArrowLeft" || dir[dir.length - 1] === "ArrowRight"))) {
+            dir.push(e.key);
+        }
+    }
+    function move() {
+        //We check to see if the next head cell in a specific direction will be outside the grid or in the snake body
+        if (dir[0] === "ArrowUp" && (snake[0].line - 1 === 0 || grid[snake[0].line - 1][snake[0].col].classList.contains("snake")) ||
+        dir[0] === "ArrowDown" && (snake[0].line + 1 === 21 || grid[snake[0].line + 1][snake[0].col].classList.contains("snake")) ||
+        dir[0] === "ArrowLeft" && (snake[0].col - 1 === 0 || grid[snake[0].line][snake[0].col - 1].classList.contains("snake")) ||
+        dir[0] === "ArrowRight" && (snake[0].col + 1 === 21 || grid[snake[0].line][snake[0].col + 1].classList.contains("snake"))) {
+            endGame();
+            window.removeEventListener("keydown", setDirection);
+        } else {
+            // we move the snake by removing the snake tail cell and updating the snake head
+            grid[snake[snake.length - 1].line][snake[snake.length - 1].col].classList.remove("snake");
+            snake.pop();
+            if (dir[0] === "ArrowUp") {
+                snake.unshift({"line": snake[0].line - 1, "col": snake[0].col});
+            } else if (dir[0] === "ArrowDown") {
+                snake.unshift({"line": snake[0].line + 1, "col": snake[0].col});
+            } else if (dir[0] === "ArrowLeft") {
+                snake.unshift({"line": snake[0].line, "col": snake[0].col - 1});            
+            } else if (dir[0] === "ArrowRight") {
+                snake.unshift({"line": snake[0].line, "col": snake[0].col + 1});
+            }
+            grid[snake[0].line][snake[0].col].classList.add("snake");
+            if (dir.length > 1) {
+                dir.shift();
+            }
+        }
+    }
+    window.addEventListener("keydown", setDirection);
+}
+
+function endGame() {
+    clearInterval(animate);
+    startButton.innerText = "Start Again";
+}
+
+startButton.addEventListener("click", startGame);
